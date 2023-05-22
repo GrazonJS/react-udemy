240f613301f3d9b540755d6e3c4dec7da8bac388
GrazonJS <grashon.2003@gmail.com>
2023-05-22 20:32:12 +0530

    first commit

---
+++ tabs-06/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState(0);
+  return <>Start</>;
+}
+
+export default App;

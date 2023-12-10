54b54f48fbd2145aae6f1e4a9a4b4e65397143f9
GrazonJS <grashon.2003@gmail.com>
2023-12-10 23:18:24 +0530

    first commit

---
+++ useContext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <h2>not logged in </h2>;
+
+  return <div>Profile : {user.userName}</div>;
+}
+
+export default Profile;
